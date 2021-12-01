8598e01cd300dc957c2fe011259220f78bfefd18
Arpit Bhalla <arpitbhalla2001@gmail.com>
2021-12-01 18:26:34 +0530

    Changes

--- index.ts
+++ index.ts
@@ -18,15 +18,15 @@ function getFiles() {
 
 const execPromise = util.promisify(exec);
 
-// const prisma = new PrismaClient();
+const prisma = new PrismaClient();
 
 const wait = (timeout = 1) => {
- return new Promise((r) =>
+  return new Promise((r) =>
     setTimeout(() => {
       r("");
     }, timeout * 1000)
   );
-}
+};
 
 const LIMIT = 5;
 const GITHUB_SHA = process.env.GITHUB_SHA || "";
@@ -35,40 +35,43 @@ const TEST_NUMBER = Number(process.env.TEST_NUMBER) || 1;
 async function main(er: Error | null, files: string[]) {
   // @Note: Some issue with this. Array is empty sometimes
   // files = files.slice(TEST_NUMBER * (LIMIT - 1), TEST_NUMBER * LIMIT);
-
+  console.log(files);
   const result = [];
 
   for (const file of files) {
-        const [_, testURL, testName] = file.match(/tests\/(.*)\/(.*).txt/) || [];
+    const [_, testURL, testName] = file.match(/tests\/(.*)\/(.*).txt/) || [];
 
-        const crusherRecorder = spawn("crusher-electron-app");
+    const crusherRecorder = spawn("crusher-electron-app", ["--no-sandbox"]);
+    console.log(testName, testURL);
+    await wait(2);
 
-        await wait(0.5);
+    await execPromise(`xmacroplay -d 50 "$DISPLAY" < ${file}`);
 
-        await execPromise(`xmacroplay -d 50 "$DISPLAY" < ${file}`);
-
-        const status = "PASS";
-        result.push({ file, status });
-        // await prisma.runResult.create({
-        //   data: {
-        //     testName,
-        //     testURL,
-        //     status,
-        //     sha: GITHUB_SHA,
-        //     releaseName: "",
-        //     logs: "",
-        //   },
-        // });
-        crusherRecorder.kill("SIGHUP");
-        await wait();
+    const status = "PASS";
+    result.push({ file, status });
+    await prisma.runResult.create({
+      data: {
+        testName,
+        testURL,
+        status,
+        sha: GITHUB_SHA,
+        releaseName: "",
+        logs: "",
+      },
+    });
+    crusherRecorder.kill("SIGHUP");
+    await wait();
   }
 
-  fs.writeFileSync("./output.txt", JSON.stringify(result), { encoding: "utf-8", flag: "w" });
+  fs.writeFileSync("./output.txt", result.join("\n"), {
+    encoding: "utf-8",
+    flag: "w",
+  });
 }
 
 (async () => {
-    // await prisma.$connect();
-    const files = await getFiles();
+  await prisma.$connect();
+  const files = await getFiles();
 
-    await main(null, files as any);
+  await main(null, files as any);
 })();
